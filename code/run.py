f11a33a6871f5c5a3425f1fea9d28a6d8e519dd2
IvanaXu <1440420407@qq.com>
2023-05-30 01:32:09 +0800

    Update c237cae63fa87816f62ec7fe5aa57203  -

---
+++ code/run.py
@@ -0,0 +1,57 @@
+import datetime
+import pandas as pd
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("data/RECORD.csv")
+
+data["DATE"] = pd.to_datetime(data["DATE"])
+data["mDATE"] = data["DATE"].dt.strftime("%Y-%m")
+data["dDATE"] = data["DATE"].dt.day
+print(data)
+print(data.describe())
+
+mth = data.groupby("mDATE").sum("VAL")
+mth = [i for i in mth[mth["VAL"] > 0].index]
+print(mth)
+
+
+INSERT = f"Update Time {datetime.datetime.now()}\n"
+for imth in tqdm(mth):
+    idata = data[data["mDATE"] == imth]
+
+    plt.figure(figsize=(9, 3))
+    plt.plot(idata["dDATE"], idata["VAL"], "r*")
+    
+    plt.xlabel("Date")
+    plt.ylabel("Value")
+    plt.title(f"{imth}")
+    plt.axis([1, 31, 6.0, 12.0])
+    plt.savefig(f"outs/DV_{imth}.png")
+
+    plt.close()
+
+    INSERT += f"\n![{imth}](outs/DV_{imth}.png)"
+
+
+BASE = """
+# GMAHealth
+
+@_@
+
+<div align=center>
+
+བཀྲ་ཤིས་བདེ་ལེགས་
+
+[![IvanaXu/GMAHealth](https://gitee.com/IvanaXu/GMAHealth/widgets/widget_card.svg?colors=4183c4,ffffff,ffffff,e3e9ed,666666,9b9b9b)](https://gitee.com/IvanaXu/GMAHealth)
+
+https://github.com/IvanaXu/GMAHealth
+
+</div>
+"""
+
+with open("README.md", "w") as f:
+    f.write(BASE.replace("@_@", INSERT))
+
+
+
---
+++ run.sh
@@ -0,0 +1,6 @@
+
+python code/run.py
+
+git add *;echo;
+git commit -m "Update $(date "+%Y%m%d%H%M%S"|md5sum)";echo;
+git push origin master;echo;
