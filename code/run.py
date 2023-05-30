15a27fd0e8b202579bb270ea0c2519f7f4d907b4
IvanaXu <1440420407@qq.com>
2023-05-30 13:06:33 +0800

    update view && data

--- code/run.py
+++ code/run.py
@@ -2,36 +2,55 @@ import datetime
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from numpy import polyfit, poly1d
 
 data = pd.read_csv("data/RECORD.csv")
 
 data["DATE"] = pd.to_datetime(data["DATE"])
+data["yDATE"] = data["DATE"].dt.strftime("%Y")
 data["mDATE"] = data["DATE"].dt.strftime("%Y-%m")
-data["dDATE"] = data["DATE"].dt.day
+data["dyDATE"] = data["DATE"].dt.dayofyear
+data["dmDATE"] = data["DATE"].dt.day
 print(data)
 print(data.describe())
 
-mth = data.groupby("mDATE").sum("VAL")
-mth = [i for i in mth[mth["VAL"] > 0].index]
-print(mth)
+dtL = data.groupby("mDATE").sum("VAL")
+dtL = set([i for i in dtL[dtL["VAL"] > 0].index])
+dtL |= set([i[:4] for i in dtL])
+dtL = sorted(dtL)
+print(dtL)
 
 
 INSERT = f"Update Time {datetime.datetime.now()}\n"
-for imth in tqdm(mth):
-    idata = data[data["mDATE"] == imth]
+for dt in tqdm(dtL):
+    [xname, dtname, dtop, dlabel, dline, dcolor] = [
+        "dyDATE", "yDATE", 366, False, 1, "blue"
+    ] if len(dt) == 4 else [
+        "dmDATE", "mDATE", 31, True, 0, "red"
+    ]
 
-    plt.figure(figsize=(9, 3))
-    plt.plot(idata["dDATE"], idata["VAL"], "r-*")
-    
+    idata = data[data[dtname] == dt]
+    x, y = idata[xname], idata["VAL"]
+
+    plt.figure(figsize=(9, 4))
+    plt.plot(x, y, marker="+", color=dcolor, linewidth=dline)
+
+    _y = poly1d(polyfit(x, y.fillna(0), 9))
+    # plt.plot(x, _y(x), "r-")
+
+    if dlabel:
+        for _x, _y in zip(x, y):
+            plt.text(_x, _y, _y, ha='center', va='bottom', fontsize=10, color="red")
+
     plt.xlabel("Date")
     plt.ylabel("Value")
-    plt.title(f"{imth}")
-    plt.axis([1, 31, 6.0, 12.0])
-    plt.savefig(f"outs/DV_{imth}.png")
+    plt.title(f"{dt}")
+    plt.axis([1, dtop, 6.0, 12.0])
+    plt.savefig(f"outs/DV_{dt}.png")
 
     plt.close()
 
-    INSERT += f"\n![{imth}](outs/DV_{imth}.png)"
+    INSERT += f"\n![{dt}](outs/DV_{dt}.png)"
 
 
 BASE = """
